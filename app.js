c927a5891c7860ccb60cad994ffb3200d3f86c64
nkthota <nkt.naveen@gmail.com>
2024-04-25 21:13:08 -0500

    latest2

--- app.js
+++ app.js
@@ -30,6 +30,32 @@ app.get('/product-metadata/:productId', cors(), async (req, res) => {
   }
 });
 
+app.put('/update-product-metafield/:productId', async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const metafieldData = {
+      metafield: {
+        id: '30168120525038',
+        value: '["Archana"]',
+        type: 'list.single_line_text_field'
+      }
+    };
+
+    // Make PUT request to Shopify's API to update the metafield
+    const response = await axios.put(`https://archanapopup.myshopify.com/admin/api/2021-10/products/${productId}/metafields/30168120525038.json`, metafieldData, {
+      headers: {
+        'X-Shopify-Access-Token': 'a8d0702d1a40bcff3405b9ba4c3ef42a',
+        'Content-Type': 'application/json'
+      }
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error updating product metafield:', error.response ? error.response.data : error.message);
+    res.status(error.response ? error.response.status : 500).json({ error: 'Failed to update product metafield' });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
